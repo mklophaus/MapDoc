55d304454dc9980eb59a482f3b4c5018ccd48fc2
Mike Klophaus <mmklophaus@gmail.com>
2015-12-15 11:37:03 -0800

    created maps controller

---
+++ public/controllers/maps_controller.js
@@ -0,0 +1,36 @@
+(function() {
+  "use strict";
+
+  angular
+      .module("mapdocApp")
+      .controller("MapsController", MapsController);
+
+  MapsController.$inject = [];
+
+  function MapsController(){
+
+    var vm = this;
+
+// "https://maps.googleapis.com/maps/api/geocode/output?parameters"
+
+      vm.maps = {
+
+        center: {
+          longitude: 40,
+          latitude: 34
+        }
+
+        zoom: 6
+      }
+
+      vm.markerList = {
+      {latitude: 34, longitude:34},
+
+      {latitude: 54, longitude: 100}
+
+      };
+  }
+
+
+})();
+
