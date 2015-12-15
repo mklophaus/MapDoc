f6dc404f2a18803169a1ea57d596529ab9f3e151
Mike Klophaus <mmklophaus@gmail.com>
2015-12-15 15:40:02 -0800

    added document routes

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ var express = require('express'),
 // Require controllers.
 var welcomeController = require('../controllers/welcome');
 var usersController   = require('../controllers/users');
+var docsController =    require('../controllers/docs');
 
 // root path:
 router.get('/', welcomeController.index);
@@ -11,5 +12,12 @@ router.get('/', welcomeController.index);
 // users resource paths:
 router.get('/users',     usersController.index);
 router.get('/users/:id', usersController.show);
+
+///DOCS ROUTES
+router.get('/docs/:id',    docsController.docShow);
+router.get('/docs',       docsController.docIndex);
+router.post('/docs',      docsController.docCreate);
+router.put('/docs/:id',    docsController.docUpdate);
+router.delete('/docs/:id', docsController.docDelete);
 
 module.exports = router;
