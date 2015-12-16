75eaa4b6d69a11c76e74170daa40ab9d7c15c5c7
Mike Klophaus <mmklophaus@gmail.com>
2015-12-16 15:48:40 -0800

    added scope for map

--- public/controllers/main_controller.js
+++ public/controllers/main_controller.js
@@ -5,15 +5,25 @@
     .module("mapdocApp")
     .controller("MainController", MainController);
 
-  MainController.$inject = ["$state", "$log"];
+  MainController.$inject = ["$scope", "$state", "$log"];
 
-  function MainController($state, $log) {
+  function MainController($scope, $state, $log) {
 
     var vm = this;
 
     vm.welcomeMessage = "Welcome, User";
 
     vm.$state = $state;
+
+    $scope.map = {
+      center: {
+        latitude:   34.04,
+        longitude: -118.25
+      },
+      zoom: 12
+    };
+
+
 
   }
 
