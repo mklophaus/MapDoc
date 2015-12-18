ba37d7ed01336a9e8f591b42ccb558b11a78cfa6
Mike Klophaus <mmklophaus@gmail.com>
2015-12-17 18:08:19 -0800

    added seed data

--- seeds.js
+++ seeds.js
@@ -1,6 +1,7 @@
 var mongoose = require('./config/database');
 
-var User = require('./models/user');
+var User = require('./models/user.js');
+var Doc = require('./models/doc.js')
 
 var users = [
   { // 0
@@ -13,6 +14,15 @@ var users = [
   }
 ];
 
+var docs = [
+  {
+    title: "RATS",
+    subject: "RODENTS",
+    location: "Pasadena, CA",
+    fileUrl: "http://www.google.com"
+  }
+];
+
 User.remove({}, function(err) {
   if (err) console.log(err);
   User.create(users, function(err, users) {
@@ -20,6 +30,18 @@ User.remove({}, function(err) {
       console.log(err);
     } else {
       console.log("Database seeded with " + users.length  + " users.");
+    }
+  });
+});
+
+
+Doc.remove({}, function(err) {
+  if (err) console.log(err);
+  Doc.create(docs, function(err, users) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database seeded with " + docs.length  + " docs.");
       mongoose.disconnect();
     }
   });
