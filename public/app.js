44fc40631f8d1a06d8180869aa578b212a04bc57
Mike Klophaus <mmklophaus@gmail.com>
2015-12-22 17:11:24 -0700

    added configuration for http

--- public/app.js
+++ public/app.js
@@ -4,6 +4,12 @@
     .module("mapdocApp", [
       "ui.router",
       "uiGmapgoogle-maps"
-    ]);
+    ])
+
+    .config(function($httpProvider) {
+  // attach our auth interceptor to the http requests
+      $httpProvider.interceptors.push('authInterceptor');
+    });
+
 
 })();
