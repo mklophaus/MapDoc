3edc5b3b4713ca03be783d39db6d4030f1a96f96
Mike Klophaus <mmklophaus@gmail.com>
2016-01-11 23:14:54 -0800

    edits

--- public/controllers/main_controller.js
+++ public/controllers/main_controller.js
@@ -16,6 +16,29 @@
     vm.isLoggedIn = authService.isLoggedIn;
 
      vm.$state = $state;
+
+
+
+
+
+    vm.login      = login;
+    vm.isLoggedIn = authService.isLoggedIn;
+    vm.currentUser = userDataService.user;
+
+    // Form data for login
+    vm.loginData;
+
+    function login() {
+      authService.login(vm.loginData.email, vm.loginData.password)
+        .then(function(res) {
+          $log.log(res.data);
+          userDataService.user = res.data.user;
+          $state.go('home');
+        });
+    };
+
+
+
   }
 
 })();
