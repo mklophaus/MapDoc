57ec4db4e11d1ca292d3b1253810e88c5d7fcde2
Mike Klophaus <mmklophaus@gmail.com>
2016-01-13 11:18:04 -0800

    dbUri edits

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var env = require('./environment');
 
 // Use different database URIs based on whether an env var exists.
-var dbUri = env.MONGOLAB_URI ||
-            'mongodb://localhost/' + env.SAFE_TITLE;
+var dbUri = process.env.MONGOLAB_URI ||
+            'mongodb://localhost/' + process.env.SAFE_TITLE;
 
 // if (!env.MONGOLAB_URI) {
 //   // check that MongoD is running...
