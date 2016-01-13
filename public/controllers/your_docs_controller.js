7387870b43c525839a531dd02263b84e64a47729
Mike Klophaus <mmklophaus@gmail.com>
2016-01-12 17:55:39 -0800

    created delete method for dashboard

--- public/controllers/your_docs_controller.js
+++ public/controllers/your_docs_controller.js
@@ -27,31 +27,81 @@
 
 });
 
-  YourDocsController.$inject = ["$scope", "$http", "userDataService"];
+  YourDocsController.$inject = ["$state","$scope", "$http", "userDataService"];
 
-function YourDocsController($scope, $http, userDataService){
+function YourDocsController($state, $scope, $http, userDataService){
 
+  var vm = this;
   var currentUser = userDataService.user;
-  var documents = [];
+  vm.items = [];
+  var documents;
 
-  console.log(userDataService.user.name);
+   // $scope.editItem = function(item) {
+   //      angular.element(document.getElementById("modal")).scope().item = item;
+   //  };
 
-    $http.get('/docs').then(function(response){
-      angular.forEach(response.data, function(e){
+    $scope.getDocs    = getDocs;
+    $scope.deleteDoc = deleteDoc;
+    // vm.updateDoc = updateDoc;
+    // vm.resetEditorm = resetEditForm;
 
-          console.log(e.author);
-          console.log(e);
+    $scope.getDocs();
 
-          if (e.author == currentUser.name){
-            documents.push(e);
-          }
+    function getDocs() {
+    documents = [];
+
+     $http.get('/docs').then(function(response){
+        angular.forEach(response.data, function(e){
+
+            if (e.author == currentUser.name){
+              documents.push(e);
+            }
+        });
+
+      },
+      function(err) {
+          console.log('ERROR!', err);
       });
-    },
-    function(err) {
-        console.log('ERROR!', err);
-    });
 
-  $scope.items = documents;
+      console.log(documents);
+
+      $scope.items = documents;
+    }
+
+    function deleteDoc(id, i) {
+      $http.delete('/docs/' + id).then(function(response) {
+        console.log(response);
+        $scope.items.splice($scope.items.indexOf(i), 1);
+
+      }, function(err) {
+        console.error('Error deleting doc!', err);
+      }).then(getDocs);
+    }
+
+    function updateDoc(id) {
+      $http.put('/docs' + id, vm.editDoc).then(function(response) {
+        vm.editDoc = {
+          author: "",
+          title: "",
+          subject: "",
+          location: ""
+        };
+      }, function(err) {
+        console.log('Error editing!', err);
+      }).then(getDocs);
+    }
+
+    function resetEditForm() {
+      vm.docTitle = '';
+      vm.docSubject = '';
+      vm.editDoc = {
+        name: "",
+        category: ""
+      };
+    }
+
+
+
 
 }
 
