48c66f69fc071a52f4e48c168976a11cc4d859d2
Mike Klophaus <mmklophaus@gmail.com>
2016-01-12 17:53:46 -0800

    Removed uibModal

--- public/controllers/users_controller.js
+++ public/controllers/users_controller.js
@@ -2,22 +2,21 @@
   angular.module('mapdocApp')
          .controller('UsersController', UsersController);
 
-  UsersController.$inject = ['$state', 'authService', 'userDataService', '$log', '$uibModal'];
+  UsersController.$inject = ['$state', 'authService', 'userDataService', '$log'];
 
-  function UsersController($state, authService, userDataService, $log, $uibModal) {
+  function UsersController($state, authService, userDataService, $log) {
     var vm = this;
 
     vm.currentUser = authService.currentUser;
     // attaching functions to controller
     vm.createUser = createUser;
 
-
-    // defining function declarations
     function createUser() {
       vm.message = '';
-      // use the create function in the userService
+
       userDataService.create(vm.userData)
         .success(function(data) {
+          console.log(vm.userData);
           vm.userData = {};
           vm.message = data.message;
           console.log(vm.message);
