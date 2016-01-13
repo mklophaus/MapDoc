c915424bf311b5dd251d6459706278d32a6d4a8f
Mike Klophaus <mmklophaus@gmail.com>
2016-01-13 10:32:28 -0800

    server edits

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ require('dotenv').load();
 // Instantiate a server application.
 var app = express();
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
+// var allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// }
 
 // Configure the application (and set it's title!).
 app.set('title', env.TITLE);
